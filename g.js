488dc4d7cd12b6a6623e8277f6c3ad193e4f076e
Ben-A-Sabu <benasabu123@gmail.com>
2023-02-14 07:09:50 +0530

    changed background

--- g.js
+++ g.js
@@ -1,79 +1,88 @@
-const sqr=document.querySelectorAll(".square");
-var mole=document.querySelectorAll(".mole");
-var timeLeft=document.querySelector("#timeLeft");
-var score=document.querySelector("#score");
-var grid=document.querySelector(".grid");
+const sqr = document.querySelectorAll(".square");
+var mole = document.querySelectorAll(".mole");
+var timeLeft = document.querySelector("#timeLeft");
+var score = document.querySelector("#score");
+var grid = document.querySelector(".grid");
+document.body.style.cursor = "url('OIP.jpg'), auto";
 
-let result=0;
+
+let result = 0;
 let hitPosition;
-let currentTime=60;
-let timer=null;
-let lasthole="";
+let currentTime = 60;
+let timer = null;
+let lasthole = "";
 
-var img = document.createElement("img");  
-img.src = "mole.png"; 
+var img = document.createElement("img");
+img.src = "mole.png";
 
 img.setAttribute("style", "margin-left: 41px;");
 
 
 
 
-function randomSquare(){    
-   
-    sqr.forEach(square=>{
-        lasthole.innerHTML=" "
-          
+function randomSquare() {
+
+    sqr.forEach(square => {
+        lasthole.innerHTML = " "
+
     })
 
 
-    let randomsqr=sqr[Math.floor(Math.random()*9)]
+    let randomsqr = sqr[Math.floor(Math.random() * 9)]
     randomsqr.appendChild(img);
- //   randomsqr.innerHTML+='<img src="'+img.src+'" />'
-   lasthole=randomsqr;
-  
-    hitPosition=randomsqr.id
-    
-   
-} 
+    //   randomsqr.innerHTML+='<img src="'+img.src+'" />'
+    lasthole = randomsqr;
+
+    hitPosition = randomsqr.id
+
+
+}
+
+
 
 
 sqr.forEach(square => {
 
-    square.addEventListener('mousedown', () =>{
 
-       if(square.id == hitPosition){
 
-         result++;
-         score.textContent=result;
-         hitPosition=null;
+    square.addEventListener('mousedown', () => {
 
-       }
-   
-   
+        if (square.id == hitPosition) {
+
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+
+        }
+
+
     })
 })
 
 
-function moveMole(){
-   
-    timer=setInterval(randomSquare,1500)
-  
+function moveMole() {
+    // add transition to the mole
+
+    timer = setInterval(randomSquare, 500)
+
 }
 
 moveMole()
 
-function countDopwn(){
-   
-currentTime--;
-timeLeft.textContent=currentTime;
-if(currentTime==0){
-    clearInterval(countdownTimerId)
-    clearInterval(timer)
-    alert("game over your final score is "+ result);
+function countDopwn() {
+
+    currentTime--;
+    timeLeft.textContent = currentTime;
+    if (currentTime == 0) {
+        clearInterval(countdownTimerId)
+        clearInterval(timer)
+        document.getElementById("GameOver_popup").style.display = "flex";
+        document.getElementById("game_Result").innerHTML = "game over your final score is " + result;
+
+
+    }
+
 
 }
 
-
-}
-
-let countdownTimerId=setInterval(countDopwn,1000);
+let countdownTimerId = setInterval(countDopwn, 1000);
